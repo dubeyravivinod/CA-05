0395a1055b959db2cef23e43ad87b14fe7117ecb
Dubey Ravi Vinod <dubeyravivinod@gmail.com>
2023-06-01 23:00:28 +0530

    First Commit

---
+++ src/components/Index.jsx
@@ -0,0 +1,36 @@
+import './Index.css'
+
+const IndexPage = () => {
+    return (
+        <div>
+            <div className="Menu">
+                <div className="logo">
+                    <h1>Logo</h1>
+                </div>
+                <div className="headerMenu">
+                    <h1>
+                        Home
+                    </h1>
+                    <h1>
+                        About
+                    </h1>
+                    <h1>
+                        Menu
+                    </h1>
+                    <h1>
+                        Order
+                    </h1>
+                    <h1>
+                        Contact
+                    </h1>
+                </div>
+            </div>
+            <div className='HOME'>
+                <h1>Home</h1>
+                <h4>Made with 💖 by Dubey Ravi Vinod</h4>
+            </div>
+        </div>
+    );
+}
+
+export default IndexPage;
